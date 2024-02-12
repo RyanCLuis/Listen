8410d4a4ef3b5d6ec0404d2e6e13dfd9206efa07
Ryan Luis <RyanCLuis@gmail.com>
2024-02-11 17:22:04 -0800

    finalizes create podcast

--- src/components/podcasts/PodcastCreate.jsx
+++ src/components/podcasts/PodcastCreate.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import PodcastForm from '../shared/PodcastForm'
+import { createPodcast } from '../../api/podcast'
+import { useNavigate } from 'react-router-dom'
 
 const PodcastCreate = (props) => {
     const { msgAlert, user } = props
+    const navigate = useNavigate()
     const [podcast, setPodcast] = useState({
         name: '',
         description: '',
@@ -29,15 +32,34 @@ const PodcastCreate = (props) => {
             const updatedPodcast = { [updatedName]: updatedValue }
             return { ...prevPodcast, ...updatedPodcast }
         })
-
     }
     console.log('the podcast inside create', podcast)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        createPodcast(podcast, user)
+            .then(res => { navigate(`/${res.data.podcast._id}`) })
+            .then(() => {
+                msgAlert({
+                    heading: 'Podcast Created Successfully',
+                    message: 'You have added a new podcast',
+                    variant: 'success',
+                })
+            })
+            .catch((error) => {
+                msgAlert({
+                    heading: 'Podcast Create Failed with error: ' + error.message,
+                    message: 'Please try again',
+                    variant: 'danger',
+                })
+            })
+    }
 
     return (
         <PodcastForm 
             podcast={podcast}
             handleChange={onChange}
-            handleSubmit={() => {console.log('handleSubmit')}}
+            handleSubmit={onSubmit}
             heading='Add a new Podcast'
         />
     )
